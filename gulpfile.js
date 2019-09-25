120f42ad829fa7d1f3cc6f5660a0353ce3d869b0
YiJiangFengYun <yijiangfengyun@outlook.com>
2019-09-25 16:07:31 +0800

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 //Import modules
 const gulp = require("gulp");
 const fs = require("fs");
+const path = require("path");
+const fsExtra = require("fs-extra");
 
 /**
  * Define constants
@@ -9,39 +11,73 @@ const nameContainerNginx = "nginx";
 const nameContainerAppCluster = "app";
 const numCluster = 2;
 const portClusters = [3000, 3001];
+const pathServer = "server";
+const pathNginx = "nginx";
+const pathBuild = "build";
+const nameDockerFile = "Dockerfile";
+
 
 
 /**
  * Define sub tasks.
  **/
 
+//Copy source files to build ouput directory.
+gulp.task("copy-files", gulp.series([
+    () => {
+        //Remove the build directory.
+        return fsExtra.remove(pathBuild);
+    },
+    () => {
+        //Copy the files in the server directory.
+        return gulp.src(path.join(process.cwd(), pathServer, "**/*"))
+            .pipe(gulp.dest(path.join(pathBuild, pathServer)));
+    },
+    () => {
+        //Copy the files in the nginx directory.
+        return gulp.src(path.join(process.cwd(), pathNginx, "**/*"))
+            .pipe(gulp.dest(path.join(pathBuild, pathNginx)));
+    },
+    () => {
+        //Copy the docker-compose.yml.
+        return gulp.src("docker-compose.yml")
+            .pipe(gulp.dest(path.join(pathBuild)));
+    }
+]))
+
 //Create Dockerfile for the app (app cluster).
 gulp.task("make-app-dockerfile", () => {
     return Promise.resolve()
     //Read template docker file.
     .then(() => {
-        
+        return readTextFile(path.join(pathServer, nameDockerFile));
+    })
+    .then((content) => {
+        return content;
+    })
+    .then((content) => {
+        return writeTextFile(path.join(pathServer, nameDockerFile));
     });
 });
 
 //Create a nginx server config for load balancing to the app cluster.
 gulp.task("make-nginx-config", () => {
-
+    return Promise.resolve();
 });
 
 //Make nginx 
 gulp.task("make-nginx-dockerfile", () => {
-
+    return Promise.resolve();
 });
 
 //Make docker compose file.
 gulp.task("make-docker-compose-file", () => {
-
+    return Promise.resolve();
 });
 
 //Run docker compose build
 gulp.task("build-docker-compose", () => {
-
+    return Promise.resolve();
 });
 
 
@@ -50,6 +86,7 @@ gulp.task("build-docker-compose", () => {
  **/
 
 gulp.task("build", gulp.series([
+    "copy-files",
     "make-app-dockerfile",
     "make-nginx-config",
     "make-nginx-dockerfile",
@@ -62,7 +99,7 @@ gulp.task("build", gulp.series([
  * Define functions
  **/
 function readTextFile(path) {
-    return Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         fs.readFile(path, "utf8", (err, data) => {
             if (err) return reject(err);
             resolve(data);
@@ -71,7 +108,7 @@ function readTextFile(path) {
 }
 
 function writeTextFile(path, data) {
-    return Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         fs.writeFile(path, data, (err) => {
             if (err) return reject(err);
             resolve();
