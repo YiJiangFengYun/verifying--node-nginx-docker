04f9a232e752d64390cb74cb8bc3615e2ee5f22d
YiJiangFengYun <yijiangfengyun@outlook.com>
2019-09-27 10:50:32 +0800

    Add server name and id

--- server/index.js
+++ server/index.js
@@ -2,17 +2,20 @@ const http = require('http');
 const args = require("args");
 
 args.option("host", "Host to listen. Default value is 0.0.0.0", "0.0.0.0")
-    .option("port", "Poart to listen. Default value is 1080", 1080);
+    .option("port", "Poart to listen. Default value is 1080", 1080)
+    .option("name", "Name for this app.", "default");
 
 const options = args.parse(process.argv);
 
 const host = options.host;
 const port = options.port;
+const name = options.name;
+const id = Math.round(Math.random() * 100000);
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  res.end(`Hello, I am ${id}!`);
 });
 
 server.listen(port, host, () => {
